47114f97a0c7c47ef729bd494c143c5ebdfc1365
Hannes Magnusson <bjori@mongodb.com>
2014-07-19 17:36:37 +0000

    wip

--- .travis.scripts/compile.sh
+++ .travis.scripts/compile.sh
@@ -65,7 +65,7 @@ sudo make install
 
 rm -rf tmp-lcov src/libmongoc # coveralls may pick it up and lie about our coverage
 
-echo "extension=phongo" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+echo "extension=phongo.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 
 MONGO=`which mongo`
 mongod --version
